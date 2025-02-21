4d17fdafbc29ee15f462357e03707f49cd7d146b
seonghyeok <chosh2001@naver.com>
2025-02-21 16:48:41 +0900

    fix: success 페이지 수정 \n\n 메인페이지 이동 버튼만 나오도록

--- src/Success.tsx
+++ src/Success.tsx
@@ -119,14 +119,19 @@ export const SuccessPage: React.FC = () => {
           </div>
         </div>
         <div className="p-grid-col">
-          <Link to="https://docs.tosspayments.com/guides/v2/payment-widget/integration">
+          <Link to="/">
+            <button className="button p-grid-col5" style={{ backgroundColor: "#3182f6", color: "#ffffff" }}>
+              메인 페이지로 이동
+            </button>
+          </Link>
+          {/* <Link to="https://docs.tosspayments.com/guides/v2/payment-widget/integration">
             <button className="button p-grid-col5">연동 문서</button>
           </Link>
           <Link to="https://discord.gg/A4fRFXQhRu">
             <button className="button p-grid-col5" style={{ backgroundColor: "#e8f3ff", color: "#1b64da" }}>
               실시간 문의
             </button>
-          </Link>
+          </Link> */}
         </div>
       </div>
       <div className="box_section" style={{ width: "600px", textAlign: "left" }}>
